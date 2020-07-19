84fd6520605be65e06b9f860b01ee02a4fca2a5e
Junaid Arshad <68495797+iamjunaidkhokhar@users.noreply.github.com>
2020-07-19 21:09:27 +0500

    Add files via upload

---
+++ signin.dart
@@ -0,0 +1,21 @@
+//use this code to make connection between flutter/Javascript and firebase for google account authentication..! 
+
+
+Future<bool> loginWithGoogle() async {
+    try {
+      GoogleSignIn googleSignIn = GoogleSignIn();
+      GoogleSignInAccount account = await googleSignIn.signIn();
+      if (account == null) return false;
+      AuthResult res = await FirebaseAuth.instance
+          .signInWithCredential(GoogleAuthProvider.getCredential(
+        idToken: (await account.authentication).idToken,
+        accessToken: (await account.authentication).accessToken,
+      ));
+      if (res.user == null) return false;
+      return true;
+    } catch (e) {
+      print(e.message);
+      print("Error logging with google");
+      return false;
+    }
+  }
